07d16a833487d1cd4d48bca3df6aadb4c36ad3f0
Gaurav Kumar <gauravkr7103@gmail.com>
2025-07-22 21:08:35 +0530

    install jest and babel
    After the TOP webpack section these were added

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,24 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
             },
+            {
+                test: /\.jsx?$/,
+                exclude: /'node_modules'/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            config$: path.resolve(__dirname, "configs/app-config.js"),
+            react: path.resolve(__dirname, "vendor/react-master"),
+        },
+        extensions: ['.js', '.jsx'],
+        modules: [
+            'node_modules',
+            'bower_components',
+            'shared',
+            '/shared/vendor/modules',
         ],
     },
 }
